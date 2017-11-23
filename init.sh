dab89a5b404cf12eb795768fd6e55ddc51882e50
dalaoshe <clyb2011@163.com>
2017-11-23 11:56:16 +0800

    modify init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
 pip install Django
-pip install 
+
+pip install djangorestframework
+pip install markdown       # Markdown support for the browsable API.
+pip install django-filter  # Filtering support
+pip install djangorestframework-jwt
